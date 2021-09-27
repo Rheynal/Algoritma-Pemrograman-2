27ca92bff0997d5ffcd9924392e79f98e30923e7
Rheynal <91449041+Rheynal@users.noreply.github.com>
2021-09-27 12:30:16 +0700

    Praktikum 1, Tugas 1

---
+++ Algoritma/Tugas 1 Praktikum 1, Muhammad Rheynal Ghazali - 064002100026.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 13:05:01 2021
+
+@author: mrhey
+NAMA  : Muhammad Rheynal Ghazali
+NIM : 064002100026
+
+
+""" 
+
+x = input("nama kamu siapa? ")
+y = input("darimana kamu berasal? ")
+v = input("sebelumnya bersekolah dimana? ")
+print("hallo selamat siang",x)
+print("Wah rupanya kamu dari",y)
+print("Pasti asyik bersekolah di",v)
